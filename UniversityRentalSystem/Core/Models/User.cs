41074766be7b2fb052324065ec286ed2b998af29
Marta Badowska <178725829+bvdovska@users.noreply.github.com>
2026-03-21 17:56:43 +0100

    feat: Add User base class with Student and Employee implementations

--- UniversityRentalSystem/Core/Models/User.cs
+++ UniversityRentalSystem/Core/Models/User.cs
@@ -2,5 +2,27 @@ namespace UniversityRentalSystem.Core.Models;
 
 public abstract class User
 {
+    public Guid Id { get; } = Guid.NewGuid();
+    public string FirstName { get; init; }
+    public string LastName { get; init; }
     
+    public abstract int MaxActiveRentals { get; }
+
+    protected User(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+    }
+}
+
+public class Student : User
+{
+    public Student(string fn, string ln) : base(fn, ln) { }
+    public override int MaxActiveRentals => 2;
+}
+
+public class Employee : User
+{
+    public Employee(string fn, string ln) : base(fn, ln) { }
+    public override int MaxActiveRentals => 5;
 }
